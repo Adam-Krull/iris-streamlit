0eac7ce526bdd755ab5948cbdbc23c6817b37730
Adam Krull <aqkrull@gmail.com>
2023-10-11 15:53:19 -0500

    decent job

--- first_pass.py
+++ first_pass.py
@@ -15,11 +15,14 @@ def viz_results(data, labels):
     
     for combo in itertools.combinations(data.columns, 2):
         
+        name = combo[0].replace('_', ' ').capitalize()
+        nametoo = combo[1].replace('_', ' ').capitalize()
+        
         fig, ax = plt.subplots()
         ax.scatter(data[combo[0]], data[combo[1]], c=labels)
-        ax.set_title(f'Combination of {combo[0]} and {combo[1]}.')
-        ax.set_xlabel(f'{combo[0]}')
-        ax.set_ylabel(f'{combo[1]}')
+        ax.set_title(f'Combination of {name} and {nametoo}')
+        ax.set_xlabel(f'{name}')
+        ax.set_ylabel(f'{nametoo}')
         st.pyplot(fig)
         
         
@@ -60,11 +63,27 @@ def form_unpacker(response):
 
 def main():
     
+    st.title("One dataset to rule them all")
+    
+    st.subheader("Clustering the Iris dataset with KMeans")
+    
+    st.divider()
+    
     df = sns.load_dataset('iris')
     
     x_df = df.drop(columns=['species'])
     
+    st.header("Acquire and prepare")
+    
+    st.markdown("The data was acquired using the seaborn library. The target variable, species, was dropped from the dataset. Below, you can see the first few observations.")
+    
     st.dataframe(x_df.head())
+    
+    st.divider()
+    
+    st.header("Modeling")
+    
+    st.markdown("The data is scaled using the MinMaxScaler before clustering. The MinMaxScaler scales all values to be from 0-1, while preserving the shape of the original data. It's important to scale data before using the KMeans clustering algorithm, which uses distance to assign data points to clusters. The KMeans algorithm will define however many clusters you specify.")
     
     with st.form(key='feature_selection'):
         
@@ -72,31 +91,27 @@ def main():
         
         form_items = [st.checkbox(col) for col in x_df.columns]
         
+        st.divider()
+        
+        st.write("How many clusters would you like to create?")
+        
+        num_clusters = st.text_input("Enter a value for k:")
+        
         submit = st.form_submit_button()
         
     cols = form_unpacker(form_items)
     
     filtered_df = df.iloc[:, cols]
     
-    with st.form(key='num_clusters'):
-        
-        st.write("How many clusters would you like to create?")
-        
-        num_clusters = st.text_input("Enter a value for k:")
-        
-        submit_two = st.form_submit_button()
-        
     try:
         
         num_clusters = int(num_clusters)
         
     except:
         
-        st.write("Please enter a valid integer for the number of clusters.")
+        pass
         
-    if submit_two:    
-        
-        st.write("We have entered the second stage of the application.")
+    if submit:    
         
         labels = fit_kmeans(filtered_df, num_clusters)    
         
